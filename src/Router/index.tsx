6d3bbcf238e485b709551d123c1810323004f153
Jay <jayashree.gunasekaran@flyerssoft.com>
2023-12-15 06:43:55 +0530

    user page

--- src/Router/index.tsx
+++ src/Router/index.tsx
@@ -3,6 +3,7 @@ import Claims from "../pages/Claims";
 import LoginScreen from "../pages/LoginScreen";
 import RegisterPage from "../pages/RegisterPage";
 import PendingApprovals from "../pages/PendingApprovals";
+import UsersPage from "../pages/Users";
 
 const RouterWrapper = () => {
   return (
@@ -11,6 +12,7 @@ const RouterWrapper = () => {
       <Route path="/claims" element={<Claims />} />
       <Route path="/pendingApprovals" element={<PendingApprovals />} />
       <Route path="/signup" element={<RegisterPage />} />
+      <Route path="/users" element={<UsersPage/>} />
     </Routes>
   );
 };
